a607e9c731a9f95ecefeb96e68f590ace1ba7c5e
David <dkesler@gmail.com>
2012-03-04 18:24:08 -0500

    Pusherrobot ai

--- discwar_ai/urls.py
+++ discwar_ai/urls.py
@@ -18,6 +18,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^discwar/aggressive$', 'discwar_ai.views.aggressive', name='game'),
+    url(r'^discwar/pusherrobot$', 'discwar_ai.views.pusher_robot', name='game'),
     url(r'^discwar$', 'discwar_ai.views.loadGame')
 )
 
--- discwar_ai/views.py
+++ discwar_ai/views.py
@@ -7,6 +7,7 @@ import math
 import time
 import random
 import os
+import traceback
 
 class cart(object):
 	def __init__(self, x, y):
@@ -38,17 +39,146 @@ def aggressive(request):
 		response['Access-Control-Allow-Methods'] = 'POST'
 		return response
 
+@csrf_exempt
+def pusher_robot(request):
+	try:
+		json_data = json.loads(request.raw_post_data)
+	
+		me = json_data['me']
+		all = json_data['all']
+		settings = json_data['settings']
+		powerups = getByType(all, 'powerup')
+		them = getEnemy(me, all)
+
+		if len(powerups) > 0 and getDistFromCenter(getNearest(me, powerups), settings) < settings['boardRadius'] - 1.3 * settings['playerRadius'] and me['mass'] < them['mass'] * 2:
+			a = goForPowerup(me, all, settings)
+		elif me['mass'] < them['mass'] or (getDistFromCenter(me, settings) > settings['boardRadius'] - 5 * settings['playerRadius'] and getDistFromCenter(me, settings) > getDistFromCenter(them, settings) and getDist(me, them) < 3 * settings['playerRadius']):
+			a = panic(me, all, settings)
+		elif getDist(me, them) < 6 * settings['playerRadius'] and getDist(me, getCenterAsObj(settings)) > 3 * settings['playerRadius']:
+			a  = zone(me, all, settings)
+		else:
+			a = goForEnemy(me, all, settings)
+
+		response = HttpResponse(json.dumps(a), mimetype="application/javascript)")
+	except Exception as e:
+		traceback.print_exc()
+		print e
+	else:
+		response['Access-Control-Allow-Origin'] = '*'
+		response['Access-Control-Allow-Methods'] = 'POST'
+		return response
+
+def panic(me, all, settings):
+	towardsEnemy = getTowardsEnemy(me, all)
+	towardsCenter = getTowards(me, getCenterAsObj(settings))
+	sideToFavor = math.copysign(1, towardsCenter.th - towardsEnemy.th)
+	orthogonal = towardsEnemy.th + sideToFavor * math.pi/4
+	if (orthogonal > 2*math.pi):
+		orthogonal -= 2*math.pi
+	
+	return {'r' : me['maxAcc'], 'th' : orthogonal}
+
+def getCenterAsObj(settings):
+	return {'x' : settings['maxWidth'] / 2, 'y' : settings['maxHeight'] / 2}
+
+def getDistFromCenter(obj, settings):
+	return getDist(obj, {'x' : settings['maxWidth'] / 2, 'y' : settings['maxHeight'] / 2})
+	
+
+def getDist(a, b):
+	return math.sqrt(math.pow(a['x'] - b['x'], 2) + math.pow(a['y'] - b['y'], 2))
+
+def getTowardsEnemy(me, all):
+	them = getEnemy(me, all);
+	return getTowards(me, them)
+
+def getTowards(me, it):
+	return cartToPol(cart(it['x'] - me['x'], it['y'] - me['y']))
+
+def goForPowerup(me, all, settings):
+	powerups = getByType(all, 'powerup');
+	if len(powerups) == 0:
+		print 'how tf did i get here?'
+		return None
+
+	nearestPowerup = getNearest(me, powerups)
+	return goForTarget(me, nearestPowerup)
+
+def goForTarget(me, target):
+	towardsTarget = getTowards(me, target)
+
+	x = me['maxAcc']
+	y = -polToCart(pol(me['v']['r'], me['v']['th'] - towardsTarget.th)).y;
+
+	new = cartToPol(cart(x, y))
+	return {'r' : new.r, 'th' : new.th + towardsTarget.th}
+
+def getRemainingAcc(r, y):
+	if math.fabs(y) > math.fabs(r) :
+		return 0
+	return math.sqrt(r*r - y*y) 
+
+def goForEnemy(me, all, settings):
+	them = getEnemy(me, all)
+	towardsEnemy = getTowardsEnemy(me, all)
+	x = me['maxAcc']
+	y = -polToCart(pol(me['v']['r'], me['v']['th'] - towardsEnemy.th)).y;
+	new = cartToPol(cart(x, y))
+	return {'r' : new.r, 'th' : new.th + towardsEnemy.th}
+
+def getPositionInBasis(cart, basis):
+	posPol = cartToPol(cart)
+	posPol.th -= basis.th
+	return polToCart(posPol)
+	
+def zone(me, all, settings):
+	them = getEnemy(me, all)
+	theirV = polToCart(pol(them['v']['r'], them['v']['th']))
+	theirA = polToCart(pol(them['a']['r'], them['a']['th']))
+	them['x'] = them['x'] + theirV.x + theirA.x
+	them['y'] = them['y'] + theirV.y + theirA.y
+
+	towardsEnemy = getTowardsEnemy(me, all)
+	centerToEnemy = getTowards(getCenterAsObj(settings), them)
+
+	myPos = getPositionInBasis(cart(me['x'] - settings['maxWidth']/2, me['y'] - settings['maxHeight'] / 2), centerToEnemy);
+	theirPos = getPositionInBasis(cart(them['x'] - settings['maxWidth']/2, them['y'] - settings['maxHeight'] / 2), centerToEnemy);
+
+	x = math.copysign(me['maxAcc'], - myPos.x + theirPos.x)
+	y = -myPos.y
+	#x = math.copysign(getRemainingAcc(me['maxAcc'], y), - myPos.x + theirPos.x)
+
+	new = cartToPol(cart(x, y))
+
+	return {'r' : new.r, 'th' : new.th + centerToEnemy.th}
+	
+
+def getNearest(me, others):
+	minDist = 9999999
+	minOther = None
+
+	for other in others:
+		dist = getDist(me, other)
+		if dist < minDist:
+			minDist = dist
+			minOther = other
+
+	return other
+		
+
 def getEnemy(me, all):
 	my_type = me['type']
 	
 	if (my_type == "player0"):
-		return getByType(all, "player1");
+		return getFirstByType(all, "player1");
 	else:
-		return getByType(all, "player0");
+		return getFirstByType(all, "player0");
 
+def getFirstByType(all, type):
+	return getByType(all, type)[0];
 
 def getByType(all, type):
-	return [x for x in all if x['type'] == type][0]
+	return [x for x in all if x['type'] == type]
 
 def cartToPol(c):
 	r = math.sqrt(c.x*c.x + c.y*c.y)
@@ -73,7 +203,7 @@ def polToCart(p):
 	x = p.r * math.cos(p.th)
 	y = p.r * math.sin(p.th)
 	
-	return {'x' : x, 'y' : y}
+	return cart(x, y)
 
 
 def loadGame(request):
